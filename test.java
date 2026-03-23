1287912c15a7e3d68ab10108c6c61e12c2724852
Samu-rgb-a <samuelinstituto07@gmail.com>
2026-03-23 17:24:34 +0100

    Add files via upload

---
+++ test.java
@@ -0,0 +1,42 @@
+import com.germangascon.ed.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player("Steve", 100, 10);
+    }
+
+    @AfterEach
+    void tearDown() {
+        player = null;
+    }
+
+    @Test
+    void curarSumaVida() {
+
+        player = new Player("Steve", 5, 10);
+        player.curar(5);
+        assertEquals(10, player.getVida());
+    }
+
+    @Test
+    void curarHastaMaximoExacto() {
+
+        player = new Player("Steve", 80, 10);
+        player.curar(20);
+        assertEquals(100, player.getVida());
+    }
+
+    @Test
+    void curarAUnMuertoNoFunciona() {
+        player.recibirDanyo(100);
+        player.curar(100);
+        assertEquals(0, player.getVida(), "Un jugador muerto no debería poder curarse.");
+    }
+}
